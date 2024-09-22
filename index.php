1af89fc534ae9b43678f04cf32f88262ca206dc2
dev-lisser <abdallah.lisser.iro@gmail.com>
2024-09-22 12:26:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,16 +12,14 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Récupération des données depuis la base de données
 $sql = "SELECT * FROM element";
 $result = $conn->query($sql);
 
-// Catégories pour les affichages
 $categories = [
     'PC' => [],
     'Routeur' => [],
     'Passerelles' => [],
-    'Imprimantes' => [] // Ajout de la catégorie imprimantes
+    'Imprimantes' => [] 
 ];
 
 if ($result->num_rows > 0) {
@@ -29,22 +27,21 @@ if ($result->num_rows > 0) {
         $ip = $row['ip'];
         $nom = $row['nom'];
 
-        // Détermine la catégorie et l'image basée sur le nom du périphérique et l'adresse IP
         if (substr($ip, -2) == '.1' && strpos($nom, '_gateway') !== false) {
             $icon = 'routeur.png';
             $categorie = 'Routeur';
-        } elseif (strpos($nom, 'DC-') === 0 || preg_match('/^LATECH[1-6]$/', $nom)) {
-            $icon = 'poste.png';  // Affiche poste.png pour les noms spécifiques
+        } elseif (strpos($nom, 'DC-') === 0 || preg_match('/^LATECH[1-6]$/', $nom) || strpos(strtolower($nom), 'mac') !== false)
+) {
+            $icon = 'poste.png';  
             $categorie = 'PC';
-        } elseif (strpos($nom, 'EPSON') !== false) {
-            $icon = 'imprimante.png';  // Affiche imprimante.png pour les imprimantes EPSON
+        } elseif (strpos($nom, 'ET') !== false) {
+            $icon = 'imprimante.png';  
             $categorie = 'Imprimantes';
         } else {
-            $icon = 'unknown.png';  // Utilise une image par défaut
-            $categorie = 'PC'; // Par défaut on met dans PC pour les inconnus
+            $icon = 'unknown.png';  
+            $categorie = 'PC'; 
         }
 
-        // Détermine si c'est une passerelle
         if (substr($ip, -4) == '.1' && $categorie !== 'Routeur') {
             $categorie = 'Passerelles';
         }
@@ -58,8 +55,8 @@ if ($result->num_rows > 0) {
     }
 }
 
-// ======= Emplacement pour le code des élèves =======
-// Les élèves peuvent insérer ici leur vue (HTML, CSS, etc.) pour afficher les données récupérées dans $categories.
+// ======= Emplacement pour le code  =======
+
 
 $conn->close();
 ?>
